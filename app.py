e4dfec8e0094ccd2950eee03dce3058354633b16
Yuh-Boh Feng <fengyuhboh@gmail.com>
2025-02-16 12:27:53 -0800

    strings

---
+++ app.py
@@ -0,0 +1,6 @@
+print("Hello World")
+print("*" * 10)
+# To Execute, type "python3 [name of file]
+# Example: python3 app.py
+
+x = 1
---
+++ primitive-types/3-strings.py
@@ -0,0 +1,25 @@
+course = "Python Programming"
+
+# len = length
+print(len(course))
+# Output: 18
+
+print(course[0])
+# Output: P
+
+print(course[-1])
+# Output: g
+
+print(course[0:3])
+# Output: Pyt
+
+print(course[0:])
+# Output: Python Programming
+
+print(course[:3])
+# Output: Pyt
+# If no int, then default is 0
+
+print(course[:])
+# Output: Python Programming
+# Will be copy of string
