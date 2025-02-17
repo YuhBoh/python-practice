5f4ca4d8cdc514ace7e11cc3d8a1f6e71d83ee53
Yuh-Boh Feng <fengyuhboh@gmail.com>
2025-02-16 18:16:32 -0800

    numbers

---
+++ primitive-types/7-numbers.py
@@ -0,0 +1,22 @@
+x = 1
+x = 1.1
+
+# a + bi
+x = 1 + 2j
+
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+
+# Rounds to nearest int
+print(10 // 3)
+
+print(10 % 3)
+print(10 ** 3)
+
+x = 10
+x = x + 3
+
+# augmented assignment operator
+x += 3
