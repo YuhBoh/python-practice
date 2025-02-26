8be33f9ffa12e2c1eb6d8200880dca22c22f1528
Yuh-Boh Feng <fengyuhboh@gmail.com>
2025-02-26 08:03:37 -0800

    added notes

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-print("Hello World")
-print("*" * 10)
-# To Execute, type "python3 [name of file]
-# Example: python3 app.py
+# print("Hello World")
+# print("*" * 10)
+# # To Execute, type "python3 [name of file]
+# # Example: python3 app.py
 
-x = 1
+# x = 1
+x = input("X: ")
+print(type(x))
+y = int(x) + 1
+print(f"x: {x}, y:{y}")
--- primitive-types/10-type-conversion.py
+++ primitive-types/10-type-conversion.py
@@ -5,6 +5,9 @@ print(type(x))
 y = int(x) + 1
 print(f"x: {x}, y: {y}")
 
+# print(f): The f is for string interpolation to replace placeholders (e.g., {x})
+#           with a value.
+
 # int(x)
 # float(x)
 # bool(x)
