b49362d52bf8716ee73939d4fa79217cb6e0b3bf
Yuh-Boh Feng <fengyuhboh@gmail.com>
2025-02-26 08:49:00 -0800

    ternary

---
+++ 2-control-flow/3-ternary-operator.py
@@ -0,0 +1,13 @@
+age = 12
+# if age >= 18:
+#   print("Eligible")
+# else:
+#   print("Not eligible")
+
+# if age >= 18:
+#   message = "Eligible"
+# else:
+#   message = "Not eligible"
+
+message = "Eligible" if age >= 18 else "Not eligible"
+print(message)
