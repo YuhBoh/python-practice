c7e538b92bd4443fdc6de6e51adbb527405a582e
Yuh-Boh Feng <fengyuhboh@gmail.com>
2025-02-26 08:34:58 -0800

    done

--- 2-control-flow/2-conditional-statements.py
+++ 2-control-flow/2-conditional-statements.py
@@ -1,6 +1,10 @@
-temperature = 35
+temperature = 15
 
 if temperature > 30:
   print("It's warm")
   print("Drink water")
+elif temperature > 20:
+  print("It's nice")
+else:
+  print("It's cold")
 print("Done")
