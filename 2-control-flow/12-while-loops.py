3b430eff3548b8141deb4de3ce7cb7b24a0923b7
Yuh-Boh Feng <fengyuhboh@gmail.com>
2025-03-05 12:39:18 -0800

    finished infinite loops

---
+++ 2-control-flow/12-while-loops.py
@@ -0,0 +1,10 @@
+# number = 100
+# while number > 0:
+#   print(number)
+#   number //= 2
+# Output: 100 50 25 12 6 3 1
+
+command = ""
+while command.lower() != "quit":
+  command = input(">")
+  print("ECHO", command)
